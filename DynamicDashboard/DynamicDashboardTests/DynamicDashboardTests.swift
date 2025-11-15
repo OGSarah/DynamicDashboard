e431c3d912d39d3214b79bf5893b3a63fbe9f4fd
Sarah Clark <sarahireneclark@gmail.com>
2025-11-14 20:58:53 -0600

    Initial commit of the Xcode project

---
+++ DynamicDashboard/DynamicDashboardTests/DynamicDashboardTests.swift
@@ -0,0 +1,17 @@
+//
+//  DynamicDashboardTests.swift
+//  DynamicDashboardTests
+//
+//  Created by Sarah Clark on 11/14/25.
+//
+
+import Testing
+@testable import DynamicDashboard
+
+struct DynamicDashboardTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
